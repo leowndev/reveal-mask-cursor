5526d94be9c420a6bbfbc22970900a8046205200
Leonardo <leonardogt2308@gmail.com>
2023-11-15 19:18:41 -0300

    Move mask based on mouse move

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,24 @@
 "use client";
 import styles from "@/app/page.module.css";
 import useMousePosition from "../app/utils/useMousePosition";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const { x, y } = useMousePosition();
 
   return (
     <main className={styles.main}>
-      <div className={styles.mask}>
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x}px ${y}px`,
+        }}
+      >
         <p className={styles.p}>
           You are now visualizing the hidden text. It was hidden behind the
           other paragraph.
         </p>
-      </div>
+      </motion.div>
 
       <div className={styles.body}>
         <p className={styles.p}>
